d5d8037fd3e65478683468ac6de7369cb11cb8a9
mridu1 <mridu@peppo.co.in>
2022-10-10 21:39:41 +0530

    reusable functions

--- app/static/js/lineGraph.js
+++ app/static/js/lineGraph.js
@@ -1,26 +1,30 @@
-function loadDetails() {
+function apiCall() {
     $.ajax({
     type: "POST",
     url: "/",
     data: {},
     success: function(result) {
-        var months = Object.keys(result);
+        var keys = Object.keys(result);
         console.log(months);
         var values = Object.values(result);
         console.log(values);
-        new Chart("myChart", {
-                type: "line",
-                data: {
-                labels: months,
-                datasets: [{
-                    fill: false,
-                    lineTension: 0,
-                    backgroundColor: "white",
-                    borderColor: "red",
-                    data: values
-                }]
-                }
-        });
+        loadDetails(keys, values);
     }
     })
+}
+
+function loadDetails(months, values) {
+    new Chart("myChart", {
+        type: "line",
+        data: {
+        labels: months,
+        datasets: [{
+            fill: false,
+            lineTension: 0,
+            backgroundColor: "white",
+            borderColor: "red",
+            data: values
+        }]
+        }
+});
 }
