1084aaad37b4f532a51f3294d8fb1c1ae454aff9
mridu1 <mridu@peppo.co.in>
2022-10-11 11:28:50 +0530

    plot NIIT stock data

--- app/app.py
+++ app/app.py
@@ -1,29 +1,21 @@
 import json
-
+import csv
 from requests import session
 from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
+filename = 'Quote-Equity-NIITLTD-EQ-10-09-2022-to-10-10-2022.csv'
 
 @app.route("/", methods=["GET", "POST"])
-def home():
-    book_sales_data = {
-        "January": 30,
-        "February": 20,
-        "March": 10,
-        "April": 50,
-        "May": 45,
-        "June": 55,
-        "July": 25,
-        "August": 60,
-        "September": 80
-    }
+def stock_data():
     if request.method == "POST":
-        print("post data received")
-        data = jsonify(book_sales_data)
-        print(type(data))
-        return data
+        data = dict()
+        with open(filename, mode='r', encoding='utf-8-sig') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                data[row['Date ']] = float(row['ltp '].replace(",", ""))
+            return data
     elif request.method == "GET":
         return render_template("linegraph.html")    
 
