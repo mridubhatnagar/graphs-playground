ca207232c2d5a2fc0900b0da9658d2e48532b041
mridu1 <mridu@peppo.co.in>
2022-10-12 17:43:11 +0530

    last 1 month stock data visualization

--- app/app.py
+++ app/app.py
@@ -5,17 +5,33 @@ from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
-filename = 'Quote-Equity-NIITLTD-EQ-11-09-2022-to-11-10-2022.csv'
+filenames = [
+    'NIITLTD.csv',
+    'INFY.csv',
+    'HCLTECH.csv'
+]
 
 @app.route("/", methods=["GET", "POST"])
 def stock_data():
     if request.method == "POST":
+        scrip_names = json.loads(request.data)['scripname']
         data = dict()
-        with open(filename, mode='r', encoding='utf-8-sig') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                data[row['Date ']] = float(row['ltp '].replace(",", ""))
-            return data
+        for scripname in scrip_names:
+            scrip_file = scripname.strip().upper() + ".csv"
+            print(scrip_file)
+            if scrip_file in filenames:
+                with open(scrip_file, mode='r', encoding='utf-8-sig') as csvfile:
+                    reader = csv.DictReader(csvfile)
+                    D = {}
+                    for row in reader:  
+                        D.update({row['Date ']: float(row['ltp '].replace(",", ""))})
+                    if D != {}:
+                        data[scripname.strip()] = D
+                    else:
+                        data = {}
+        data = jsonify(data)
+        print(data)
+        return data
     elif request.method == "GET":
         return render_template("linegraph.html")    
 
--- app/static/js/lineGraph.js
+++ app/static/js/lineGraph.js
@@ -1,32 +1,77 @@
 function apiCall() {
+    var companyName = document.getElementById('cname').value;
+    var scripName = companyName.split(",");
+    console.log(scripName);
     $.ajax({
     type: "POST",
     url: "/",
-    data: {},
+    contentType: "application/json; charset=utf-8",
+    data: JSON.stringify({scripname: scripName}),
     success: function(result) {
-        var keys = Object.keys(result);
-        var values = Object.values(result);
-        createChart(keys, values);
+        // console.log(result);
+        createChart(result);
     }
     })
 }
 
-function createChart(months, values) {
-    new Chart("myChart", {
-        type: "line",
-        data: {
-        labels: months,
-        datasets: [{
+function createChart(result) {
+    console.log(result)
+    var keys = Object.keys(result);
+    console.log(keys);
+    var values = Object.values(result);
+    console.log(values);
+    var dataset = [];
+    for (i=0; i<keys.length; i++) {
+        var dates = Object.keys(values[i]);
+        let ltp = Object.values(values[i]);
+        console.log(dates, ltp);
+        r = Math.floor(Math.random() * 255);
+        g = Math.floor(Math.random() * 255);
+        b = Math.floor(Math.random() * 255);
+        dataset.push({
             fill: false,
+            label: keys[i],
             lineTension: 0,
-            backgroundColor: "white",
-            borderColor: "red",
-            data: values
-        }]
-        },
-        options: {
-            responsive:true,
-            legend: {display: true},
-          }
-});
+            backgroundColor: `rgb(${r}, ${g}, ${b})`,
+            borderColor: `rgb(${r}, ${g}, ${b})`,
+            data: ltp
+        })
+    }
+    if (dataset.length != 0) {
+        new Chart("myChart", {
+            type: "line",
+            data: {
+            labels: dates,
+            datasets: dataset
+            },
+            options: {
+                responsive: true,
+                legend: {display: true},
+                scales: {
+                    yAxes: [{
+                        scaleLabel: {
+                          display: true,
+                          labelString: 'LTP',
+                          fontColor: "red"
+                        }
+                      }],
+                    xAxes: [{
+                        scaleLabel: {
+                          display: true,
+                          labelString: 'Last 1 Month Dates',
+                          fontColor: "red",
+                        }
+                      }]  
+                    }     
+            }
+        })
+        document.getElementById("div4").innerHTML = "";
+    } 
+    else {
+        document.getElementById("div4").innerHTML = "Entered scripname doesn't exists.";
+        const canvas = document.getElementById('myChart');
+        const context = canvas.getContext('2d');
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        // document.getElementById("chart-container").innerHTML = "";
+    }
 }
